d717db9f0b11218e8beebff2c6b796723691eca8
mlavi16 <maya.lavi11@gmail.com>
2022-12-26 14:02:06 +0200

    Initial Commit

---
+++ OneColorOut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneColorOut
+//
+//  Created by Galia Kaufman on 12/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
